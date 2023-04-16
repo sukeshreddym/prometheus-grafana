37a2ef9531735f27b9bf091007a2649abbd8bdcc
sukeshreddym <130910908+sukeshreddym@users.noreply.github.com>
2023-04-16 19:42:23 +0530

    Update grafana.sh
    adding SSM connection, install telnet , nc, net-tools, httpd

--- prometheus-grafana/grafana.sh
+++ prometheus-grafana/grafana.sh
@@ -7,5 +7,9 @@ tar -xvzf grafana-9.2.1.linux-amd64.tar.gz
 chmod -R 755 grafana-9.2.1
 cd grafana-9.2.1/bin/
 nohup ./grafana-server & 
+yum install telnet nc net-tools httpd -y
+yum install -y https://s3.amazonaws/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+systemctl start amazon-ssm-agent
+systemctl status amazzon-ssm-agent
 
 
