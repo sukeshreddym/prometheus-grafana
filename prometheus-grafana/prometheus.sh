5087e660b8f728c323c874843937d94d1523c5b7
sukeshreddym <130910908+sukeshreddym@users.noreply.github.com>
2023-04-16 19:26:30 +0530

    Update prometheus.sh
    adding ssm

--- prometheus-grafana/prometheus.sh
+++ prometheus-grafana/prometheus.sh
@@ -3,4 +3,8 @@ sudo su -
 wget https://github.com/prometheus/prometheus/releases/download/v2.39.1/prometheus-2.39.1.linux-amd64.tar.gz
 tar -xf prometheus-2.39.1.linux-amd64.tar.gz
 cd prometheus-2.39.1.linux-amd64/
- ./prometheus
+ nohup ./prometheus &
+ yum install telnet nc net-tools httpd -y
+ yum install -y https://s3.amazonaws/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+ systemctl start amazon-ssm-agent
+ systemctl status amazzon-ssm-agent
